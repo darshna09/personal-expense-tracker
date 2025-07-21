257f157c3db55524d95758551d347064a37878c8
darshna <41057192+darshna09@users.noreply.github.com>
2025-07-21 13:43:03 +0530

    Express setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-// Get started
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello, TypeScript and Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
